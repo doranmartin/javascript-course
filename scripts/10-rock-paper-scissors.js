71a89f8047f01cea2a63a6b7a136b0e660e3e970
doranmartin <doranmartin1994@gmail.com>
2023-05-08 15:39:40 -0400

    Finished Lesson 10

---
+++ scripts/10-rock-paper-scissors.js
@@ -0,0 +1,102 @@
+
+let score = JSON.parse(localStorage.getItem('score')) || {
+  wins: 0,
+  losses: 0,
+  ties: 0
+};
+
+updateScoreElement();
+
+function playGame(playerMove) {
+  computerMove = pickComputerMove();
+
+  result = '';
+
+  if (playerMove === 'scissors') {
+    if (computerMove === 'rock') {
+      result = 'You lose.';
+    }
+    else if (computerMove === 'paper') {
+      result = 'You win.';
+    }
+    else {
+      result = 'Tie.';
+    }
+  }
+  else if (playerMove === 'paper') {
+    if (computerMove === 'rock') {
+      result = 'You win.';
+    }
+    else if (computerMove === 'paper') {
+      result = 'Tie.';
+    }
+    else {
+      result = 'You lose.';
+    }
+  }
+  else {
+    if (computerMove === 'rock') {
+      result = 'Tie.';
+    }
+    else if (computerMove === 'paper') {
+      result = 'You lose.';
+    }
+    else {
+      result = 'You win.';
+    }
+  }
+
+  if (result === 'You win.') {
+    score.wins++;
+  }
+  else if (result === 'You lose.') {
+    score.losses++;
+  }
+  else {
+    score.ties++;
+  }
+
+  localStorage.setItem('score', JSON.stringify(score));
+
+  updateScoreElement();
+
+  updateMovesElement(playerMove, computerMove);
+
+  updateResultElement();
+}
+
+function updateResultElement() {
+  document.querySelector('.js-result').innerHTML = result;
+}
+
+function updateMovesElement(playerMove, computerMove) {
+  document.querySelector('.js-moves').innerHTML = `You
+<img src="images/${playerMove}-emoji.png"
+class="move-icon">
+<img src="images/${computerMove}-emoji.png"
+class="move-icon">
+Computer`;
+}
+
+function updateScoreElement() {
+  document.querySelector('.js-score').innerHTML = 
+    `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
+
+function pickComputerMove() {
+  const randomNumber = Math.random();
+
+  let computerMove = '';
+
+  if (randomNumber < 1/3) {
+    computerMove = 'rock';
+  }
+  else if (randomNumber < 2/3) {
+    computerMove = 'paper';
+  }
+  else {
+    computerMove = 'scissors';
+  }
+
+  return computerMove;
+}
