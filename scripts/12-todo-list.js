0c333aa953323759e784da4b5bceeb63b8b8f547
doranmartin <doranmartin1994@gmail.com>
2023-05-09 11:47:08 -0400

    Completed Part 1 of the course

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -11,20 +11,28 @@ renderTodoList();
 function renderTodoList() {
   let todoListHTML = '';
 
-  todoList.forEach(function(todoObject, index) {
+  todoList.forEach((todoObject, index) => {
     const { name, dueDate } = todoObject;
     const html = `
       <div>${name}</div>
       <div>${dueDate}</div>
-      <button onclick="
-        todoList.splice(${index}, 1);
-        renderTodoList();
-      " class="delete-todo-button">Delete</button>
+      <button class="delete-todo-button js-delete-todo-button">Delete</button>
     `;
     todoListHTML += html;
   });
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+  document.querySelectorAll('.js-delete-todo-button')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+      });
+    });
 }
+
+document.querySelector('.js-add-todo-button').addEventListener('click', () => {
+  addTodo();
+});
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
